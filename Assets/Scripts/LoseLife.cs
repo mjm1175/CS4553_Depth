6c2d0b198b34019285861670b2c1a6a01cf3afd9
Maria <mjm1175@nyu.edu>
2022-02-16 22:18:11 -0500

    player hit and death animations

--- Assets/Scripts/LoseLife.cs
+++ Assets/Scripts/LoseLife.cs
@@ -13,15 +13,30 @@ public class LoseLife : MonoBehaviour
     public GameObject life1;
     public GameObject life2;
     public GameObject life3;
+
+    // for shake
+    private Vector3 _startPos;
+    private float _timer;
+    private Vector3 _randomPos;
+    
+    public float _time = 0.2f;
+    public float _distance = 0.1f;
+    public float _delayBetweenShakes = 0f;
+    
     public void Hit(){
+        _startPos = transform.position;
+        StopAllCoroutines();
+        StartCoroutine(Shake());
         switch (GlobalVars.lives)
         {
             case 1:
                 if (GlobalVars.shieldOn == true) break; // don't lose life if shield is on 
                 GlobalVars.lives--;
                 life3.SetActive(false);
+                StopAllCoroutines();
+                StartCoroutine(FlashAndSpin());
                 //Debug.Log("life 3");
-                gameObject.SetActive(false);
+                //gameObject.SetActive(false);
                 Invoke("LoadGameOver", 1f); // call the LoadGameOver() after a short delay of 1 sec
                 break;
             case 2:
@@ -39,6 +54,47 @@ public class LoseLife : MonoBehaviour
             //default:
         }
     }
+
+    // Envoked when life is lost
+   private IEnumerator Shake()
+   {
+       _timer = 0f;
+ 
+       while (_timer < _time)
+       {
+           _timer += Time.deltaTime;
+ 
+           _randomPos = _startPos + (Random.insideUnitSphere * _distance);
+ 
+           transform.position = _randomPos;
+ 
+           if (_delayBetweenShakes > 0f)
+           {
+               yield return new WaitForSeconds(_delayBetweenShakes);
+           }
+           else
+           {
+               yield return null;
+           }
+       }
+ 
+       transform.position = _startPos;
+   }
+
+   private IEnumerator FlashAndSpin(){
+       Renderer thisRenderer = gameObject.GetComponent<Renderer>();
+       int i = 0;
+       Color normalColor = thisRenderer.material.color;
+       Color flashColor = new Color(1,0,0,1);
+       thisRenderer.material.color = flashColor;
+       while (true){
+            transform.Translate(-Vector3.up * 0.5f* Time.deltaTime, Space.World);
+            transform.Rotate(Vector3.forward, 700*Time.deltaTime);
+           i++;
+           yield return null;
+       }
+       //thisRenderer.material.color = normalColor;
+   }
 
     void LoadGameOver() // load game over scene after third life is lost
     {
